4ea9a75f158593de0dd65c756b7df4d90db80728
leshrike <leo.roman.vilaplana@gmail.com>
2022-09-19 21:35:04 +0200

    semi terminado

--- UD01A01-sample/UD01A01/src/main/java/es/cipfpbatoi/damb/ad/procesador/rutas/ProcesadorRutas.java
+++ UD01A01-sample/UD01A01/src/main/java/es/cipfpbatoi/damb/ad/procesador/rutas/ProcesadorRutas.java
@@ -1,32 +1,123 @@
 package es.cipfpbatoi.damb.ad.procesador.rutas;
 
 import java.io.File;
+import java.io.FileFilter;
 
 public class ProcesadorRutas {
-	
+
 	public static StringBuilder procesarRuta(ConfiguracionBusqueda configuracionBusqueda) {
-		
+
 		StringBuilder resultadoProceso = new StringBuilder();
-		
+
+		String rutaInicial = configuracionBusqueda.getRutaInicial();
+
 		resultadoProceso.append("Vamos a buscar en la ruta: " + configuracionBusqueda.getRutaInicial() + "\n");
-		
-			if (configuracionBusqueda.checkDirectorio(configuracionBusqueda.getRutaInicial())) {
-				
-				resultadoProceso.append("Procesando directorio...\n");
-				
-				File [] listadoArchivos = configuracionBusqueda.listaArchivos(configuracionBusqueda.getRutaInicial());
-				
-				for (File archivo : listadoArchivos) {
+
+		if (checkDirectorio(rutaInicial)) {
+
+			if (configuracionBusqueda.isBuscarRecursivamente()) {
+
+				File[] carpetas = getDirectorios(rutaInicial);
+
+				for (File carpeta : carpetas) {
+
+					resultadoProceso.append("Directorio: " + carpeta.getName() + ". \n");
 					
-					resultadoProceso.append("Se ha encontrado un fichero en la ruta: " + archivo.getPath() + ", con el nombre " + archivo.getName() + " y de tipo " + archivo.getName().lastIndexOf(".")+". \n");
+					getDirectorios(carpeta.getAbsolutePath());
 				}
-									
+
 			} else {
-				
-				resultadoProceso.append("La ruta introducida corresponde a un fichero \n");
+
+				File[] archivos = getFicherosFiltrados(rutaInicial, configuracionBusqueda.getFiltroNombre(),
+						configuracionBusqueda.isMostrarArchivosOcultos());
+
+				for (File archivo : archivos) {
+
+					resultadoProceso.append("El archivo " + archivo.getName() + " se encuentra en " + archivo.getPath()
+							+ " y es de tipo: " + getExtension(archivo.getPath()) + ". \n");
+				}
 			}
-			
+		}
 		return resultadoProceso;
 	}
+	
+	public static File[] getFicherosFiltrados(String ruta, String filtro, boolean ocultos) {
 
+		File archivo = new File(ruta);
+
+		FileFilter archivosFiltrados = new FileFilter() {
+
+			public boolean accept(File archivo) {
+
+				if (archivo.isFile() && archivo.exists() && archivo.getName().contains(filtro)) {
+
+					if (!ocultos && !archivo.isHidden()) {
+
+						return true;
+
+					} else if (ocultos && !archivo.isHidden() || archivo.isHidden()) {
+
+						return true;
+					}
+				}
+				return false;
+			}
+		};
+
+		File[] archivos = archivo.listFiles(archivosFiltrados);
+
+		return archivos;
+	}
+
+	public static File[] getDirectorios(String ruta) {
+
+		File carpeta = new File(ruta);
+		FileFilter listadoCarpetas = new FileFilter() {
+
+			public boolean accept(File carpeta) {
+				if (carpeta.exists() && carpeta.isDirectory()) {
+
+					return true;
+				}
+				return false;
+			}
+
+		};
+
+		File[] carpetas = carpeta.listFiles(listadoCarpetas);
+
+		return carpetas;
+	}
+
+	public static String getExtension(String ruta) {
+
+		String extension = "";
+
+		File archivo = new File(ruta);
+
+		if (archivo.getName().lastIndexOf(".") >= 0) {
+
+			extension = archivo.getPath().substring(archivo.getPath().lastIndexOf(".") + 1);
+		}
+
+		return extension;
+	}
+
+	public static boolean checkDirectorio(String ruta) {
+
+		File archivo = new File(ruta);
+
+		boolean esCarpeta;
+
+		if (!archivo.exists() && !archivo.isDirectory()) {
+
+			esCarpeta = false;
+			return esCarpeta;
+
+		} else {
+			esCarpeta = true;
+			return esCarpeta;
+		}
+
+	}
 }
